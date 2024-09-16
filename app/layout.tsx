239c76506448945e3e06c462668662450fa1d9e9
bayramovmurad <demonseytandir@gmail.com>
2024-09-16 11:05:03 +0400

    project setup

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from 'next'
+import {Inter, Roboto} from 'next/font/google'
+import './globals.css'
+import Navbar from "@/components/shared/Navbar";
+
+const roboto = Roboto({ subsets: ['latin'], weight: ["100","400","700","900"] })
+
+export const metadata: Metadata = {
+  title: "Qerb",
+  description: 'Qerb haqqinda xeberler ve meqaleler',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.className} overflow-x-hidden bg-light`}>
+      <Navbar/>
+      {children}
+      </body>
+    </html>
+  )
+}
+
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+const Home  = () => {
+    return (
+        <div className="bg-red-500">
+            App
+        </div>
+    )
+}
+
+export default Home
+
